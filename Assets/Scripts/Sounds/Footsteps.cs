605b7c672411c17f09c8913ed83eeea7e9e0e604
ethan <eppenney@upei.ca>
2024-10-28 20:35:59 -0300

    Added sound effects for footsteps

---
+++ Assets/Scripts/Sounds/Footsteps.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using UnityEngine;
+
+public class FootstepManager : MonoBehaviour {
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioClip[] clips;
+    [SerializeField] private float footstepInterval = 0.5f;
+    [SerializeField] private float runInterval = 0.25f;
+    [SerializeField] private float walkVolume = 0.5f;
+    [SerializeField] private float runVolume = 0.7f;
+    private CharacterController characterController;
+    private UnityEngine.AI.NavMeshAgent navMeshAgent;
+
+    private void Start() {
+        characterController = GetComponent<CharacterController>();
+        navMeshAgent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        audioSource = GetComponent<AudioSource>();
+        StartCoroutine(PlayFootstepSounds());
+    }
+
+    private IEnumerator PlayFootstepSounds() {
+        while (true) {
+            if (characterController.isGrounded && characterController.velocity.magnitude > 0.1f) {
+                bool running = false;
+                if (characterController != null) {
+                    running = characterController.velocity.magnitude > 5f;
+                }
+                else if (navMeshAgent != null) {
+                    running = navMeshAgent.velocity.magnitude > 5f;
+                }
+
+                float currentInterval = running ? runInterval : footstepInterval;
+                float volume = running? runVolume : walkVolume;
+
+                AudioClip clip = clips[Random.Range(0, clips.Length)];
+                audioSource.PlayOneShot(clip, volume);
+
+                yield return new WaitForSeconds(currentInterval);
+            }
+            else
+            {
+                yield return null; // Wait until the next frame to check again
+            }
+        }
+    }
+}
